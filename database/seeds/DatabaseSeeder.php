fdd4ef1e1dfd50a6468b5b65d806c7b88e084dc8
Niklas Canisius <niklascanisius@me.com>
2020-03-23 23:59:32 +0100

    stress test seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,13 +10,13 @@ class DatabaseSeeder extends Seeder
 {
     public function run() : void
     {
-        factory(ApproveToken::class, 942)->create()->each(static function (ApproveToken $approve_token) {
+        factory(ApproveToken::class, 3000)->create()->each(static function (ApproveToken $approve_token) {
             $approve_token->receiveStations()->save(factory(ReceiveStation::class)->make());
         });
-        factory(ApproveToken::class, 942)->create()->each(static function (ApproveToken $approve_token) {
+        factory(ApproveToken::class, 3000)->create()->each(static function (ApproveToken $approve_token) {
             $approve_token->launchSites()->save(factory(LaunchSite::class)->make());
         });
-        factory(ApproveToken::class, 942)->create()->each(static function (ApproveToken $approve_token) {
+        factory(ApproveToken::class, 10000)->create()->each(static function (ApproveToken $approve_token) {
             $approve_token->hunters()->save(factory(Hunter::class)->make());
         });
     }
