cc677298c607c6090665080e43735c68333cb517
Niklas Canisius <niklascanisius@me.com>
2020-03-24 00:15:38 +0100

    psr12

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,14 +10,20 @@ class DatabaseSeeder extends Seeder
 {
     public function run() : void
     {
-        factory(ApproveToken::class, 3000)->create()->each(static function (ApproveToken $approve_token) {
-            $approve_token->receiveStations()->save(factory(ReceiveStation::class)->make());
-        });
-        factory(ApproveToken::class, 3000)->create()->each(static function (ApproveToken $approve_token) {
-            $approve_token->launchSites()->save(factory(LaunchSite::class)->make());
-        });
-        factory(ApproveToken::class, 10000)->create()->each(static function (ApproveToken $approve_token) {
-            $approve_token->hunters()->save(factory(Hunter::class)->make());
-        });
+        factory(ApproveToken::class, 3000)->create()->each(
+            static function (ApproveToken $approve_token) {
+                $approve_token->receiveStations()->save(factory(ReceiveStation::class)->make());
+            }
+        );
+        factory(ApproveToken::class, 3000)->create()->each(
+            static function (ApproveToken $approve_token) {
+                $approve_token->launchSites()->save(factory(LaunchSite::class)->make());
+            }
+        );
+        factory(ApproveToken::class, 10000)->create()->each(
+            static function (ApproveToken $approve_token) {
+                $approve_token->hunters()->save(factory(Hunter::class)->make());
+            }
+        );
     }
 }
