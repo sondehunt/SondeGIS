9a73e8d50f070f0cde032bddc57faac4dc2735ec
Niklas Canisius <niklascanisius@me.com>
2020-03-23 20:17:28 +0100

    add asset cache busting

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
 </head>
 
 <body>
@@ -482,6 +482,6 @@
 <script type="text/javascript">
   window.api_url="{{ env('APP_URL') }}/api";
 </script>
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.disableNotifications()
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+
+if (mix.inProduction()) {
+    mix.version()
+}
